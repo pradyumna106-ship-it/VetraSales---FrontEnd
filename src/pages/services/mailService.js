9b0e9d11337cdb911b869746dcdaf816bc5488c3
pr4dyumn4 <pradyumnajekumar@gmail.com>
2025-12-12 15:23:27 +0530

    added notification service

---
+++ src/pages/services/mailService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_URL = "https://localhost:8080/api/notification";
+
+// -----------------------
+//  SEND EMAIL
+// -----------------------
+export const sendEmail = async (mailData) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/email`, mailData);
+    return response.data; // "Email sent successfully!"
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
+
+// -----------------------
+//  SEND SMS
+// -----------------------
+export const sendSMS = async (smsData) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/sms`, smsData);
+    return response.data; // "SMS sent!"
+  } catch (error) {
+    console.error("Error sending SMS:", error);
+    throw error;
+  }
+};
