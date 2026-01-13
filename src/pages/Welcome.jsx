3190d00704950146fb8da5ce85f6c3a71da798e4
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-01-13 14:20:53 +0530

    fixed image logo

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -32,7 +32,7 @@ export default function Welcome() {
           {/* Logo / Brand */}
           <div className="flex items-center gap-2">
             <img
-              src="https://inherent-copper-qi3fxgu3xx.edgeone.app/svgviewer-png-output.png"
+              src="src\assets\svgviewer-png-output.png"
               alt="Vetra Sales"
               className="w-10 h-10 object-contain"
             />
--- src/pages/components/Zero.jsx
+++ src/pages/components/Zero.jsx
@@ -22,7 +22,7 @@ const item = {
         <div className="grid md:grid-cols-2 gap-8 items-center">
           <motion.div variants={container} initial="hidden"animate="show" className="space-y-6 text-left">
             <motion.img
-              src="https://inherent-copper-qi3fxgu3xx.edgeone.app/svgviewer-png-output.png"
+              src="src\assets\svgviewer-png-output.png"
               alt="Superman Fly"
               initial={{ x: "-120%", opacity: 1 }}
               animate={{ x: "120%", opacity: 0 }}
