c1d26756fee4df99bfb99147dabd22e2dfab1984
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-02-04 12:12:47 +0530

    header design modified

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -30,33 +30,20 @@ export function Header({ onCartClick, onNavigate, onSearchClick, currentPage, ro
 
           {/* ================= CUSTOMER NAV ================= */}
           {role === 'customer' && (
-            <div>
-              <nav className="flex items-center justify-between px-6 h-16">
-                <Button
-                  onClick={() => onNavigate('home')}
-                  className={currentPage === 'home' ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}
-                >
-                  Home
+            
+              <><nav className="hidden md:flex items-center gap-6">
+              <Button onClick={() => onNavigate('home')} className={currentPage === 'home' ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}> Home </Button>
+              <Button onClick={() => onNavigate('products')} className={currentPage === 'products' ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}> Products</Button>
+              <Button onClick={() => onScroll('about')} className="hover:text-[#D9C88A]">  About </Button>
+              <Button onClick={() => onScroll('contact')} className="hover:text-[#D9C88A]">  Contact </Button>
+            </nav><div className="flex items-center gap-4">
+                <Button onClick={() => onSearchClick()} className="p-2 hover:bg-gray-100 rounded-full transition-colors">
+                  <Search className="w-5 h-5" />
                 </Button>
-                <Button  onClick={() => onNavigate('products')}  className={currentPage === 'products' ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}> Products</Button>
-                  <Button onClick={() => onScroll('about')}  className="hover:text-[#D9C88A]">
-                      About </Button>
-                  <Button onClick={() => onScroll('contact')} className="hover:text-[#D9C88A]">
-                    Contact </Button>
-              </nav>
-
-              <div className="flex items-center gap-4">
-                <Button onClick={() => onSearchClick()}  className="p-2 hover:bg-gray-100 rounded-full transition-colors">
-                    <Search className="w-5 h-5" />
-                </Button>
-
                 <Button className="p-2 hover:bg-gray-100 rounded-full" onClick={() => onNavigate('favourites')}>
                   <Heart className="w-5 h-5" />
                 </Button>
-                <Button
-                  onClick={onCartClick}
-                  className="relative p-2 hover:bg-gray-100 rounded-full"
-                >
+                <Button onClick={onCartClick} className="relative p-2 hover:bg-gray-100 rounded-full">
                   <ShoppingCart className="w-5 h-5" />
                   {cartCount > 0 && (
                     <span className="absolute -top-1 -right-1 bg-[#D9C88A] text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
@@ -67,27 +54,27 @@ export function Header({ onCartClick, onNavigate, onSearchClick, currentPage, ro
                 <Button className="p-2 hover:bg-gray-100 rounded-full transition-colors" onClick={() => onNavigate('user-profile')}>
                   <User className="w-5 h-5" />
                 </Button>
-              </div>
-            </div>
+              </div></>
           )}
 
           {/* ================= ADMIN NAV ================= */}
             {role === 'admin' && (
-              <div className='justify-center'>
-                <nav className="hidden md:flex item-center gap-6">
-                <Button onClick={() => onNavigate("dashboard")} className={currentPage === 'dashboard' ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}>Dashboard</Button>
-                <Button onClick={() => onNavigate("reviews")} className={currentPage === "reviews" ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}>Customer Reviews</Button>
-                <Button onClick={() => onNavigate("orders")} className={currentPage === "orders" ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}>Orders</Button>
-                <Button onClick={() => onNavigate("users")} className={currentPage === "users" ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}>User Management</Button>
-                <Button onClick={() => onSearchClick()}  className="p-2 hover:bg-gray-100 rounded-full transition-colors">
-                    <Search className="w-5 h-5" />
-                </Button>
-                <Button onClick={() => onNavigate('my-profile')} className={currentPage === "my-profile" ? 'text-[#8A7432]' : 'p-2 hover:bg-gray-100 rounded-full transition-colors'}>
-                  <User className="w-5 h-5" />
-                </Button>
-                
-              </nav>
-              </div>
+              <>
+                <nav className="hidden md:flex justify-center gap-6">
+                  <Button onClick={() => onNavigate("dashboard")} className={currentPage === 'dashboard' ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}>Dashboard</Button>
+                  <Button onClick={() => onNavigate("reviews")} className={currentPage === "reviews" ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}>Customer Reviews</Button>
+                  <Button onClick={() => onNavigate("orders")} className={currentPage === "orders" ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}>Orders</Button>
+                  <Button onClick={() => onNavigate("users")} className={currentPage === "users" ? 'text-[#8A7432]' : 'hover:text-[#D9C88A]'}>User Management</Button>
+                </nav>
+                <div className="flex items-center gap-4">
+                  <Button onClick={() => onSearchClick()}  className="p-2 hover:bg-gray-100 rounded-full transition-colors">
+                      <Search className="w-5 h-5" />
+                  </Button>
+                  <Button onClick={() => onNavigate('my-profile')} className={currentPage === "my-profile" ? 'text-[#8A7432]' : 'p-2 hover:bg-gray-100 rounded-full transition-colors'}>
+                    <User className="w-5 h-5" />
+                  </Button>
+                </div>
+              </>
           )}
 
         </div>
